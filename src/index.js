4cdfad2f7ff98569017ebb932a03ed7c6b1f9e67
Euan Williams <euanwilliams00@gmail.com>
2022-11-23 17:07:45 +0000

    Adding project file structure, website header, and navigation.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-console.log("Hello World!")
+// import your modules
+import './css/reset.css'
+import './css/style.css';
+import './modules/elements/header'
+import { createHeader } from './modules/elements/header';
+import { createNavMain } from './modules/elements/nav';
+
+const body = document.querySelector("body");
+body.append(
+    createHeader(),
+    createNavMain()
+)
